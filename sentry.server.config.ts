863165828701d9b1246180b1a5a1d5ea32e0f9d0
Manoj <manojpradhan1803@gmail.com>
2024-08-14 01:30:57 +0530

    Create a real time document where user came and make room and create, edit, delete documents

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://69dcb1444ddd429869cfb6d38bf8c71b@o4507771475722240.ingest.us.sentry.io/4507771478736896",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
